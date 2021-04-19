6fe55b20cd5c65348f4f69519b5f76890ccf7d5d
st18rai <st18rai@gmail.com>
2021-04-19 11:20:37 +0300

    added widget tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,102 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_test_app/main.dart';
+import 'package:flutter_test_app/ui/bloc/gif_bloc.dart';
+import 'package:flutter_test_app/ui/bloc/gif_state.dart';
+import 'package:flutter_test_app/ui/page/home_screen.dart';
+import 'package:mockito/mockito.dart';
+
+class MockGifBloc extends Mock implements GifBloc {}
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  // ignore: close_sinks
+  MockGifBloc _bloc;
+  HomeScreenData _screenData;
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  setUp(() {
+    _bloc = MockGifBloc();
+    when(_bloc.skip(any)).thenAnswer((_) async* {
+      null;
+    });
+    _screenData = HomeScreenData();
   });
+
+  tearDown(() {
+    _bloc.close();
+  });
+
+  Future _createWidget(WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: BlocProvider<GifBloc>(
+            create: (context) => _bloc,
+            child: HomeScreen(_bloc, _screenData),
+          ),
+        ),
+      ),
+    );
+    await tester.pump();
+  }
+
+  testWidgets('check initial text', (WidgetTester tester) async {
+    when(_bloc.state).thenReturn(GifInitialState());
+
+    await _createWidget(tester);
+
+    expect(find.text(_screenData.initialText), findsOneWidget,
+        reason: 'Should be: "Start searching gifs!"');
+
+    expect(find.text(_screenData.hintText), findsOneWidget,
+        reason: 'Should be: "Enter a search term"');
+  });
+
+  testWidgets('check enter search query text', (WidgetTester tester) async {
+    when(_bloc.state).thenReturn(GifInitialState());
+
+    final query = 'test';
+
+    await _createWidget(tester);
+
+    await tester.enterText(find.byType(TextField), query);
+
+    expect(find.text(query), findsOneWidget, reason: 'Should be: "test"');
+  });
+
+  testWidgets('check error state', (WidgetTester tester) async {
+    when(_bloc.state).thenReturn(GifFailureState());
+
+    await _createWidget(tester);
+
+    expect(find.text(_screenData.errorText), findsOneWidget,
+        reason: 'Should be: "Something went wrong"');
+  });
+
+  testWidgets('check nothing found text', (WidgetTester tester) async {
+    when(_bloc.state).thenReturn(GifSuccessState(gifs: [], hasMore: false));
+
+    await _createWidget(tester);
+
+    expect(find.text(_screenData.nothingFoundText), findsOneWidget,
+        reason: 'Should be: "Nothing found"');
+  });
+
+  // testWidgets('check gifs loaded', (WidgetTester tester) async {
+  //   _screenData.gifs = <Data>[
+  //     Data(
+  //         images: Images(
+  //             downsized: Downsized(
+  //                 url:
+  //                     'https://media2.giphy.com/media/8ByB3sXbFcmpxzz3s0/giphy.gif?cid=ecf05e47b02kvbmyxrztqf449a5pb2t0jqxjvn2hquo7qoim&rid=giphy.gif&ct=g'))),
+  //   ];
+  //
+  //   when(_bloc.state)
+  //       .thenReturn(GifSuccessState(gifs: _screenData.gifs, hasMore: true));
+  //
+  //   await _createWidget(tester);
+  //
+  //   expect(find.byType(ListView), findsOneWidget,
+  //       reason: 'Should be: listView with gifs');
+  // });
 }
